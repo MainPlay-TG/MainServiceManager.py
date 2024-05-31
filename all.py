ae1900c8a97083d01a5a78e97e79cdb4b07ca766
MainPlay-YT <xbox.roman6666666666@gmail.com>
2024-05-31 21:26:58 +0700

    no-comment

---
+++ all.py
@@ -0,0 +1,16 @@
+import os
+from shutil import which
+from time import sleep
+from subprocess import Popen
+package = "MainServiceManager"
+os.chdir(os.path.dirname(__file__))
+for i in os.listdir("src"):
+  if os.path.exists(f"src/{i}/__init__.py"):
+    Popen(["nano", f"src/{i}/__init__.py"]).wait()
+    if which("autopep8"):
+      Popen(["autopep8", "-r", "--in-place", f"src/{i}"]).wait()
+Popen(["nano", "pyproject.toml"]).wait()
+Popen(["poetry", "publish", "--build"]).wait()
+Popen(["pip", "install", "-U", package]).wait()
+sleep(10)
+Popen(["pip", "install", "-U", package]).wait()
